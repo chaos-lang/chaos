c01c47d4c6efe0797f751f9b8d44af6424af4645
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2020-09-15 16:49:38 +0300

    fix(loop): Set InteractiveShellLoopErrorAbsorber jumper

--- interpreter/loop.c
+++ interpreter/loop.c
@@ -11,13 +11,21 @@ extern int yyparse();
 ASTNode* startTimesDo(unsigned long long iter, bool is_infinite, ASTNode* ast_node) {
     ASTNode* next_node;
 
-    if (is_infinite) {
-        while (true) {
-            next_node = eval_node(ast_node->next, ast_node->module);
+    if (is_interactive) {
+        if (setjmp(InteractiveShellLoopErrorAbsorber)) {
+            interactive_shell_loop_error_absorbed = true;
         }
-    } else {
-        for (unsigned long long i = 0; i < iter; i++) {
-            next_node = eval_node(ast_node->next, ast_node->module);
+    }
+
+    if (!interactive_shell_loop_error_absorbed) {
+        if (is_infinite) {
+            while (true) {
+                next_node = eval_node(ast_node->next, ast_node->module);
+            }
+        } else {
+            for (unsigned long long i = 0; i < iter; i++) {
+                next_node = eval_node(ast_node->next, ast_node->module);
+            }
         }
     }
 
@@ -31,11 +39,19 @@ ASTNode* startForeach(char *list_name, char *element_name, ASTNode* ast_node) {
     if (list->type != K_LIST)
         throw_error(E_NOT_A_LIST, list_name);
 
-    for (unsigned long i = 0; i < list->children_count; i++) {
-        Symbol* child = list->children[i];
-        Symbol* clone_symbol = createCloneFromSymbol(element_name, child->type, child, child->secondary_type);
-        next_node = eval_node(ast_node->next, ast_node->module);
-        removeSymbol(clone_symbol);
+    if (is_interactive) {
+        if (setjmp(InteractiveShellLoopErrorAbsorber)) {
+            interactive_shell_loop_error_absorbed = true;
+        }
+    }
+
+    if (!interactive_shell_loop_error_absorbed) {
+        for (unsigned long i = 0; i < list->children_count; i++) {
+            Symbol* child = list->children[i];
+            Symbol* clone_symbol = createCloneFromSymbol(element_name, child->type, child, child->secondary_type);
+            next_node = eval_node(ast_node->next, ast_node->module);
+            removeSymbol(clone_symbol);
+        }
     }
 
     return eval_node(next_node->next, ast_node->module);
@@ -48,14 +64,22 @@ ASTNode* startForeachDict(char *dict_name, char *element_key, char *element_valu
     if (dict->type != K_DICT)
         throw_error(E_NOT_A_DICT, dict_name);
 
-    for (unsigned long i = 0; i < dict->children_count; i++) {
-        Symbol* child = dict->children[i];
+    if (is_interactive) {
+        if (setjmp(InteractiveShellLoopErrorAbsorber)) {
+            interactive_shell_loop_error_absorbed = true;
+        }
+    }
 
-        addSymbolString(element_key, child->key);
-        Symbol* clone_symbol = createCloneFromSymbol(element_value, child->type, child, child->secondary_type);
-        next_node = eval_node(ast_node->next, ast_node->module);
-        removeSymbol(clone_symbol);
-        removeSymbolByName(element_key);
+    if (!interactive_shell_loop_error_absorbed) {
+        for (unsigned long i = 0; i < dict->children_count; i++) {
+            Symbol* child = dict->children[i];
+
+            addSymbolString(element_key, child->key);
+            Symbol* clone_symbol = createCloneFromSymbol(element_value, child->type, child, child->secondary_type);
+            next_node = eval_node(ast_node->next, ast_node->module);
+            removeSymbol(clone_symbol);
+            removeSymbolByName(element_key);
+        }
     }
 
     return eval_node(next_node->next, ast_node->module);
