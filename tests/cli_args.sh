07fc33598a1fb84f5133ba24a29cdc20bf4684c9
M. Mert Yildiran <me@mertyildiran.com>
2020-12-05 22:55:26 +0300

    fix(cli-args): Also test the existence of generated files after the compilation commands

--- tests/cli_args.sh
+++ tests/cli_args.sh
@@ -9,18 +9,46 @@ echo "exit" | chaos && echo -e "\nOK\n\n" && \
 echo "exit" | chaos -d && echo -e "\nOK\n\n" && \
 
 echo -e "\nINFO: Test compilation variants with short options\n"
-chaos -c tests/number.kaos && echo -e "\nOK\n\n" && \
-chaos -c tests/number.kaos -o number && echo -e "\nOK\n\n" && \
-chaos -c tests/number.kaos -o number -e "-ggdb" && echo -e "\nOK\n\n" && \
-chaos -c tests/number.kaos -o number -e "-ggdb" -k && echo -e "\nOK\n\n" && \
-chaos -c tests/number.kaos -o number -e "-ggdb" -kd && echo -e "\nOK\n\n" && \
+chaos -c tests/number.kaos && \
+    [ -f build/main ] && \
+    echo -e "\nOK\n\n" && \
+chaos -c tests/number.kaos -o number && \
+    [ -f build/number ] && \
+    echo -e "\nOK\n\n" && \
+chaos -c tests/number.kaos -o number -e "-ggdb" && \
+    [ -f build/number ] && \
+    echo -e "\nOK\n\n" && \
+chaos -c tests/number.kaos -o number -e "-ggdb" -k && \
+    [ -f build/number ] && \
+    [ -f build/number.c ] && \
+    [ -f build/number.h ] && \
+    echo -e "\nOK\n\n" && \
+chaos -c tests/number.kaos -o number -e "-ggdb" -kd && \
+    [ -f build/number ] && \
+    [ -f build/number.c ] && \
+    [ -f build/number.h ] && \
+    echo -e "\nOK\n\n" && \
 
 echo -e "\nINFO: Test compilation variants with long options\n"
-chaos --compile tests/number.kaos && echo -e "\nOK\n\n" && \
-chaos --compile tests/number.kaos --output number && echo -e "\nOK\n\n" && \
-chaos --compile tests/number.kaos --output number --extra "-ggdb" && echo -e "\nOK\n\n" && \
-chaos --compile tests/number.kaos --output number --extra "-ggdb" --keep && echo -e "\nOK\n\n" && \
-chaos --compile tests/number.kaos --output number --extra "-ggdb" --keep --debug && echo -e "\nOK\n\n" && \
+chaos --compile tests/number.kaos && \
+    [ -f build/main ] && \
+    echo -e "\nOK\n\n" && \
+chaos --compile tests/number.kaos --output number && \
+    [ -f build/number ] && \
+    echo -e "\nOK\n\n" && \
+chaos --compile tests/number.kaos --output number --extra "-ggdb" && \
+    [ -f build/number ] && \
+    echo -e "\nOK\n\n" && \
+chaos --compile tests/number.kaos --output number --extra "-ggdb" --keep && \
+    [ -f build/number ] && \
+    [ -f build/number.c ] && \
+    [ -f build/number.h ] && \
+    echo -e "\nOK\n\n" && \
+chaos --compile tests/number.kaos --output number --extra "-ggdb" --keep --debug && \
+    [ -f build/number ] && \
+    [ -f build/number.c ] && \
+    [ -f build/number.h ] && \
+    echo -e "\nOK\n\n" && \
 
 echo -e "\nINFO: Test invalid argument messages with short options\n"
 chaos -c || echo -e "\nOK\n\n" && \
