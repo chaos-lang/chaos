96889d5d8eef662ddf95d4624fa7c4cb6da7cce0
Noah Altunian <nbaltunian@gmail.com>
2020-12-27 15:48:12 +0000

    Remove unused variable; Change flag
    The flag was changed because `! -z' is the same as `-n'. Using the
    latter is the correct way to do this.

--- requirements.sh
+++ requirements.sh
@@ -6,23 +6,22 @@ if [[ "$OSTYPE" == "linux"* ]]; then
     YUM_CMD=$(which yum)
     DNF_CMD=$(which dnf)
     PACMAN_CMD=$(which pacman)
-    PKG_CMD=$(which pkg)
     APK_CMD=$(which apk)
 
-    if [[ ! -z $APT_CMD ]]; then
+    if [[ -n $APT_CMD ]]; then
         apt update && \
         apt install -y build-essential rsync clang bison flex libreadline-dev
-    elif [[ ! -z $APT_GET_CMD ]]; then
+    elif [[ -n $APT_GET_CMD ]]; then
         apt-get update && \
         apt-get install -y build-essential rsync clang bison flex libreadline-dev
-    elif [[ ! -z $YUM_CMD ]]; then
+    elif [[ -n $YUM_CMD ]]; then
         yum install -y rsync clang flex bison readline-devel
-    elif [[ ! -z $DNF_CMD ]]; then
+    elif [[ -n $DNF_CMD ]]; then
         dnf install -y rsync clang flex bison readline-devel
-    elif [[ ! -z $PACMAN_CMD ]]; then
+    elif [[ -n $PACMAN_CMD ]]; then
         pacman -Syu --noconfirm && \
         pacman -S --noconfirm base-devel rsync clang bison flex readline
-    elif [[ ! -z $APK_CMD ]]; then
+    elif [[ -n $APK_CMD ]]; then
         apk update && \
         apk add build-base rsync clang bison flex readline-dev
     else
