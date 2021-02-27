1feee1c64220896f676d382141e209940095c1da
M. Mert Yildiran <me@mertyildiran.com>
2021-02-28 01:01:45 +0300

    Update the copyright notice in the header comments

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -1,7 +1,7 @@
 /*
  * Description: Compiler module of the Chaos Programming Language's source
  *
- * Copyright (c) 2019-2020 Chaos Language Development Authority <info@chaos-lang.org>
+ * Copyright (c) 2019-2021 Chaos Language Development Authority <info@chaos-lang.org>
  *
  * License: GNU General Public License v3.0
  * This program is free software: you can redistribute it and/or modify
