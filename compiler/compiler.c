be5bd44881cb94e84c242ddd4a48745e8f83e73c
M. Mert Yildiran <me@mertyildiran.com>
2021-04-04 00:32:00 +0300

    Fix the issues in IndexExpr for lists and update list.out

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -196,6 +196,10 @@ void compileStmt(i64_array* program, Stmt* stmt)
                 push_instr(program, INC);
                 push_instr(program, R3B);
 
+                push_instr(program, LII);
+                push_instr(program, R0B);
+                push_instr(program, V_INT);
+
                 push_instr(program, ADD);
                 push_instr(program, R3B);
                 push_instr(program, R4B);
@@ -654,23 +658,28 @@ unsigned short compileExpr(i64_array* program, Expr* expr)
             push_instr(program, program->size + 9);
 
             push_instr(program, MOV);
-            push_instr(program, R4A);
+            push_instr(program, R5A);
             push_instr(program, R7B);
 
             push_instr(program, ADD);
-            push_instr(program, R4A);
+            push_instr(program, R5A);
             push_instr(program, R2A);
 
             push_instr(program, MOV);
             push_instr(program, R2A);
+            push_instr(program, R5A);
+
+            push_instr(program, MOV);
             push_instr(program, R4A);
+            push_instr(program, R2A);
 
             push_instr(program, LII);
             push_instr(program, R3A);
             push_instr(program, -1);
 
-            push_instr(program, POP);
+            push_instr(program, LII);
             push_instr(program, R0A);
+            push_instr(program, V_INT);
 
             push_instr(program, ADD);
             push_instr(program, R2A);
@@ -680,13 +689,16 @@ unsigned short compileExpr(i64_array* program, Expr* expr)
             push_instr(program, R2A);
             push_instr(program, R3A);
 
+            push_instr(program, POP);
+            push_instr(program, R0A);
+
             push_instr(program, JEZ);
             push_instr(program, program->size + 3);
 
             push_instr(program, DPOP);
 
             push_instr(program, JNZ);
-            push_instr(program, program->size - 13);
+            push_instr(program, program->size - 16);
 
             push_instr(program, POP);
             push_instr(program, R1A);
